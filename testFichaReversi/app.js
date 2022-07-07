e772124111918fbf0b3998554ef7bb5fb0a873da
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-07-07 20:19:49 -0300

    add states to testFichaReversi

--- testFichaReversi/app.js
+++ testFichaReversi/app.js
@@ -1,8 +1,12 @@
-window.addEventListener("load", () => {
-  const $btn = document.querySelector(".toggle-btn");
-  const $piece = document.querySelector(".piece");
+const states = ['', 'black', 'white', 'can-put-black', 'can-put-white']
+let count = 0
 
-  $btn.addEventListener("click", () => {
-    $piece.dataset.piece = $piece.dataset.piece === "black" ? "white" : "black";
-  });
-});
+window.addEventListener('load', () => {
+  const $btn = document.querySelector('.toggle-btn')
+  const $piece = document.querySelector('.piece')
+
+  $btn.addEventListener('click', () => {
+    count = (count + 1) % states.length
+    $piece.dataset.piece = states[count]
+  })
+})
