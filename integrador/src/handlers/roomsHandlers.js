527795a3b81d53d579d0bfd337d32e910a2f0c6f
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-07-15 14:17:22 -0300

    create project structure

---
+++ integrador/src/handlers/roomsHandlers.js
@@ -0,0 +1,7 @@
+module.exports = (io, socket) => {
+  socket.on('room:create', () => {})
+
+  socket.on('room:join', () => {})
+
+  socket.on('room:connect', () => {})
+}
---
+++ integrador/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const httpServer = require('http').createServer(app)
+const io = require('socket.io')(httpServer)
+
+const roomsHandlers = require('./handlers/roomsHandlers')
+
+//* Settings
+app.set('port', process.env.PORT || 3000)
+
+//* Middlewares
+app.use(express.static('public'))
+
+//* Sockets
+io.on('connection', (socket) => {
+  roomsHandlers(io, socket)
+})
+
+//* Start server
+httpServer.listen(app.get('port'), () => {
+  console.log(`Server listening on http://localhost:${app.get('port')} \n\n`)
+})
