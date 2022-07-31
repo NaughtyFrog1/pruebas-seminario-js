9a41f78d2183d7a577d758cb9362f44fe9e25071
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-07-31 15:12:18 -0300

    update ship-preview and render ships on board

--- battleship-board/app.js
+++ battleship-board/app.js
@@ -39,6 +39,7 @@ $inputShip.addEventListener('input', handleInputShipChange)
 $inputDirection.addEventListener('input', handleInptutShipDirectionChange)
 $inputRow.addEventListener('input', handleInputPositionChange)
 $inputCol.addEventListener('input', handleInputPositionChange)
+$btnConfirmShip.addEventListener('click', handleBtnConfirmShip)
 
 // Disable writing
 $inputRow.addEventListener('keypress', (e) => e.preventDefault())
@@ -80,30 +81,29 @@ function renderBoard(board) {
   }
 }
 
-function renderShipPreview(ship, direction) {
-  const shipDiv = document.createElement('div')
-  shipDiv.classList.add('bb-ship')
-  if (direction === 'vertical') {
-    shipDiv.classList.add('bb-ship--vertical')
-  }
+function renderShipPreview(parentNode, ship, direction) {
+  const container = document.createElement('div')
+  container.classList.add('bb-ship-preview', `bb-ship-preview--${direction}`)
 
   for (let i = 0; i < SHIPS[ship]; i++) {
+    const shipContainer = document.createElement('div')
+    shipContainer.classList.add('bb-ship', `bb-ship--${direction}`)
+
     const shipBody = document.createElement('div')
     shipBody.classList.add('bb-ship__body')
-    shipDiv.appendChild(shipBody)
-  }
-  shipDiv.children[0].classList.add('bb-ship__body--tail')
-  shipDiv.children[shipDiv.children.length - 1].classList.add(
-    'bb-ship__body--head'
-  )
 
-  $shipPreview.innerHTML = ''
-  $shipPreview.appendChild(shipDiv)
+    shipContainer.appendChild(shipBody)
+    container.appendChild(shipContainer)
+  }
+  container.firstChild.firstChild.classList.add('bb-ship__body--tail')
+  container.lastChild.firstChild.classList.add('bb-ship__body--head')
+  parentNode.innerHTML = ''
+  parentNode.appendChild(container)
 }
 
-function moveShipPreview(row, col) {
-  $shipPreview.style.top = `${row * SQUARE_SIZE}px`
-  $shipPreview.style.left = `${col * SQUARE_SIZE}px`
+function moveShipPreview(parentNode, row, col) {
+  parentNode.style.top = `${row * SQUARE_SIZE}px`
+  parentNode.style.left = `${col * SQUARE_SIZE}px`
 }
 
 function updateInputPositionMax(ship, direction) {
@@ -116,29 +116,82 @@ function updateInputPositionMax(ship, direction) {
   }
 }
 
+function renderShipOnSquare(square, direction, part = '') {
+  const shipContainer = document.createElement('div')
+  if (direction === 'horizontal') {
+    shipContainer.classList.add('bb-ship--horizontal')
+  } else if (direction === 'vertical') {
+    shipContainer.classList.add('bb-ship--vertical')
+  }
+
+  const shipBody = document.createElement('div')
+  shipBody.classList.add('bb-ship__body')
+  if (part === 'head') {
+    shipBody.classList.add('bb-ship__body--head')
+  } else if (part === 'tail') {
+    shipBody.classList.add('bb-ship__body--tail')
+  }
+
+  shipContainer.appendChild(shipBody)
+  square.appendChild(shipContainer)
+}
+
+function renderShipOnBoard(board, ship, direction, rowStart, colStart) {
+  const parsedRow = parseInt(rowStart, 10)
+  const parsedCol = parseInt(colStart, 10)
+
+  if (direction === 'horizontal') {
+    const shipTailPos = parsedRow * SQUARES + parsedCol
+    const shipHeadPos = parsedRow * SQUARES + parsedCol + SHIPS[ship] - 1
+    renderShipOnSquare(board.children[shipTailPos], direction, 'tail')
+    renderShipOnSquare(board.children[shipHeadPos], direction, 'head')
+    for (let colOffset = 1; colOffset < SHIPS[ship] - 1; colOffset++) {
+      const shipPos = parsedRow * SQUARES + parsedCol + colOffset
+      renderShipOnSquare(board.children[shipPos], direction)
+    }
+  } else if (direction === 'vertical') {
+    const shipHeadPos = parsedRow * SQUARES + parsedCol
+    const shipTailPos = (parsedRow + SHIPS[ship] - 1) * SQUARES + parsedCol
+    renderShipOnSquare(board.children[shipTailPos], direction, 'tail')
+    renderShipOnSquare(board.children[shipHeadPos], direction, 'head')
+    for (let rowOffset = 1; rowOffset < SHIPS[ship] - 1; rowOffset++) {
+      const shipPos = (parsedRow + rowOffset) * SQUARES + parsedCol
+      renderShipOnSquare(board.children[shipPos], direction)
+    }
+  }
+}
+
+//* Event handlers
+
 function handleInputShipChange() {
   $inputRow.value = shipPositionsState[$inputShip.value].row
   $inputCol.value = shipPositionsState[$inputShip.value].col
   $inputDirection.value = shipPositionsState[$inputShip.value].direction
 
-  renderShipPreview($inputShip.value)
   updateInputPositionMax($inputShip.value, $inputDirection.value)
-  moveShipPreview($inputRow.value, $inputCol.value)
+  renderShipPreview($shipPreview, $inputShip.value, $inputDirection.value)
+  moveShipPreview($shipPreview, $inputRow.value, $inputCol.value)
 }
 
 function handleInptutShipDirectionChange() {
-  if ($inputDirection.value === 'horizontal') {
-    $shipPreview.firstElementChild.classList.remove('bb-ship--vertical')
-  } else if ($inputDirection.value === 'vertical') {
-    $shipPreview.firstElementChild.classList.add('bb-ship--vertical')
-  }
   $inputRow.value = 0
   $inputCol.value = 0
 
   updateInputPositionMax($inputShip.value, $inputDirection.value)
-  moveShipPreview($inputRow.value, $inputCol.value)
+  renderShipPreview($shipPreview, $inputShip.value, $inputDirection.value)
+  moveShipPreview($shipPreview, $inputRow.value, $inputCol.value)
 }
 
 function handleInputPositionChange() {
-  moveShipPreview($inputRow.value, $inputCol.value)
+  moveShipPreview($shipPreview, $inputRow.value, $inputCol.value)
+}
+
+function handleBtnConfirmShip() {
+  renderShipOnBoard(
+    $playerBoard,
+    $inputShip.value,
+    $inputDirection.value,
+    $inputRow.value,
+    $inputCol.value
+  )
 }
